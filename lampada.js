43d63282533d122fc4bec41c60a617d41df85df8
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-09-05 21:26:14 -0300

    Atualização

---
+++ lampada.js
@@ -0,0 +1,20 @@
+const turnOn = document.getElementById('turnOn')
+const turnOff = document.getElementById('turnOff')
+const lamp = document.getElementById('lamp')
+
+function lampOn () {
+  lamp.src = './img/lampadacesa.jpg'
+
+}
+
+function lampOff () {
+  lamp.src = './img/lampada.jpg'
+}
+
+function lampBroken () {
+  lamp.src = './img/lampadaquebrada.jpg'
+}
+
+turnOn.addEventListener('click', lampOn)
+turnOff.addEventListener('click', lampOff)
+lamp.addEventListener("dblclick", lampBroken)
